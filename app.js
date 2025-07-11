f2001581397c5ce2a15bed855e7427b994617b44
shubhamagrawal100 <shubhamkalyanagrawal@gmail.com>
2025-07-11 19:40:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,10 @@ const verifyToken = process.env.VERIFY_TOKEN;
 
 // Route for GET requests
 app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
-
-  if (mode === 'subscribe' && token === verifyToken) {
+ 
     console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
-  } else {
-    res.status(403).end();
-  }
+
 });
 
 // Route for POST requests
